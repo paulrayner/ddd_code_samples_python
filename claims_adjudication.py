03911039ea4945ccf4bd97ac7de0e07c51e114da
Paul Rayner <paul@virtualgenius.com>
2021-09-28 14:37:43 -0600

    Make adjudicate a class method

--- claims_adjudication.py
+++ claims_adjudication.py
@@ -11,7 +11,8 @@ from product import Product
 # after comparing claims to the benefit or coverage requirements.
 
 class ClaimsAdjudication:
-  def adjudicate(contract, new_claim):
+  @classmethod
+  def adjudicate(cls, contract, new_claim):
     claim_total = sum(claim.amount for claim in contract.claims)
     if((contract.purchase_price - claim_total) * 0.8 > new_claim.amount) and contract.status == "ACTIVE" and new_claim.failure_date >= contract.effective_date and new_claim.failure_date <= contract.expiration_date:
         contract.claims.append(new_claim)
