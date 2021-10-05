d6817d5a2655c3ce4556f096b63f9b228446db31
Paul Rayner <paul@virtualgenius.com>
2021-10-05 12:25:06 -0600

    Replace datetime with date

--- test/test_claims_adjudication.py
+++ test/test_claims_adjudication.py
@@ -9,24 +9,24 @@ class TestAdjudicateValidClaim(unittest.TestCase):
 
     def fake_contract(self):
         product  = Product("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
-        contract = Contract(100.0, product, datetime.datetime(2010, 5, 8), datetime.datetime(2013, 5, 8), datetime.datetime(2010, 5, 7))
+        contract = Contract(100.0, product, datetime.date(2010, 5, 8), datetime.date(2013, 5, 8), datetime.date(2010, 5, 7))
         contract.status = "ACTIVE"
         return contract
 
     def test_adjudicate_valid_claim(self):
         contract = self.fake_contract()
 
-        claim = Claim(79.0, datetime.datetime(2010, 5, 8))
+        claim = Claim(79.0, datetime.date(2010, 5, 8))
 
         ClaimsAdjudication.adjudicate(contract, claim)
         self.assertEqual(len(contract.claims), 1)
         self.assertEqual(contract.claims[0].amount, 79.0)
-        self.assertEqual(contract.claims[0].failure_date, datetime.datetime(2010, 5, 8))
+        self.assertEqual(contract.claims[0].failure_date, datetime.date(2010, 5, 8))
 
     def test_adjudicate_claim_with_invalid_amount(self):
         contract = self.fake_contract()
 
-        claim = Claim(81.0, datetime.datetime(2010, 5, 8))
+        claim = Claim(81.0, datetime.date(2010, 5, 8))
 
         ClaimsAdjudication.adjudicate(contract, claim)
         self.assertEqual(len(contract.claims), 0)
@@ -35,7 +35,7 @@ class TestAdjudicateValidClaim(unittest.TestCase):
         contract = self.fake_contract()
         contract.status = "PENDING"
 
-        claim = Claim(79.0, datetime.datetime(2010, 5, 8))
+        claim = Claim(79.0, datetime.date(2010, 5, 8))
 
         ClaimsAdjudication.adjudicate(contract, claim)
         self.assertEqual(len(contract.claims), 0)
@@ -44,7 +44,7 @@ class TestAdjudicateValidClaim(unittest.TestCase):
         contract = self.fake_contract()
         contract.status = "EXPIRED"
 
-        claim = Claim(79.0, datetime.datetime(2010, 5, 8))
+        claim = Claim(79.0, datetime.date(2010, 5, 8))
 
         ClaimsAdjudication.adjudicate(contract, claim)
         self.assertEqual(len(contract.claims), 0)
@@ -52,7 +52,7 @@ class TestAdjudicateValidClaim(unittest.TestCase):
     def test_adjudicate_claim_prior_to_effective_date(self):
         contract = self.fake_contract()
 
-        claim = Claim(79.0, datetime.datetime(2010, 5, 5))
+        claim = Claim(79.0, datetime.date(2010, 5, 5))
 
         ClaimsAdjudication.adjudicate(contract, claim)
         self.assertEqual(len(contract.claims), 0)
@@ -60,7 +60,7 @@ class TestAdjudicateValidClaim(unittest.TestCase):
     def test_adjudicate_claim_after_expiration_date(self):
         contract = self.fake_contract()
 
-        claim = Claim(79.0, datetime.datetime(2013, 5, 9))
+        claim = Claim(79.0, datetime.date(2013, 5, 9))
 
         ClaimsAdjudication.adjudicate(contract, claim)
         self.assertEqual(len(contract.claims), 0)
--- test/test_contract.py
+++ test/test_contract.py
@@ -9,21 +9,21 @@ class TestContract(unittest.TestCase):
 
     def test_contract_is_set_up_correctly(self):
         product  = Product("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
-        contract = Contract(100.0, product, datetime.datetime(2010, 5, 8), datetime.datetime(2013, 5, 8), datetime.datetime(2010, 5, 7))
+        contract = Contract(100.0, product, datetime.date(2010, 5, 8), datetime.date(2013, 5, 8), datetime.date(2010, 5, 7))
 
         self.assertTrue(hasattr(contract, "id"))
         self.assertEqual(contract.purchase_price, 100.0)
         self.assertEqual(contract.status, "PENDING")
         self.assertEqual(Product("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0"), contract.product)
-        self.assertEqual(contract.purchase_date, datetime.datetime(2010, 5, 7))
-        self.assertEqual(contract.effective_date, datetime.datetime(2010, 5, 8))
-        self.assertEqual(contract.expiration_date, datetime.datetime(2013, 5, 8))
+        self.assertEqual(contract.purchase_date, datetime.date(2010, 5, 7))
+        self.assertEqual(contract.effective_date, datetime.date(2010, 5, 8))
+        self.assertEqual(contract.expiration_date, datetime.date(2013, 5, 8))
 
     def test_contract_equality(self):
         product  = Product("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
-        contract1 = Contract(100.0, product, datetime.datetime(2010, 5, 8), datetime.datetime(2013, 5, 8), datetime.datetime(2010, 5, 7))
-        contract2 = Contract(100.0, product, datetime.datetime(2010, 5, 8), datetime.datetime(2013, 5, 8), datetime.datetime(2010, 5, 7))
-        contract3 = Contract(100.0, product, datetime.datetime(2010, 5, 8), datetime.datetime(2013, 5, 8), datetime.datetime(2010, 5, 7))
+        contract1 = Contract(100.0, product, datetime.date(2010, 5, 8), datetime.date(2013, 5, 8), datetime.date(2010, 5, 7))
+        contract2 = Contract(100.0, product, datetime.date(2010, 5, 8), datetime.date(2013, 5, 8), datetime.date(2010, 5, 7))
+        contract3 = Contract(100.0, product, datetime.date(2010, 5, 8), datetime.date(2013, 5, 8), datetime.date(2010, 5, 7))
 
 
         expected_id = uuid.uuid1()
